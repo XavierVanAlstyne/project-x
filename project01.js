65c196100d2ae79175a46cb8f3908bb5a647e06d
XavierVanAlstyne <xvvanalstyne@gmail.com>
2020-12-23 16:40:59 -0500

    finish project

--- project01.js
+++ project01.js
@@ -9,3 +9,15 @@
   what each data type is.
 */
 
+var number = 2020;
+console.log(typeof number);
+var string = "this is a string";
+console.log(typeof string);
+var object = ['this', 'is', 'an', 'object'];
+console.log(typeof object);
+var boolean = true;
+console.log(typeof boolean);
+var undefined;
+console.log(typeof undefined);
+var nul = null;
+console.log(typeof nul);

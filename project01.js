0f13855ef9989ac391c1bf13729ff793be6973a8
Eric Gray <ericlamargray@gmail.com>
2020-12-23 12:22:22 -0500

    project 1

---
+++ project01.js
@@ -0,0 +1,11 @@
+ /* 
+  What is a Data Type?
+ */
+ 
+ /*
+  Javascript has 6 main data types. Use this file
+  to assign each data type to a variable and
+  use javascript's built in function to console.log
+  what each data type is.
+*/
+

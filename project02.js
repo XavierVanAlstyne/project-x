3556e85185c61a79f7d7eb74c938996af87d6cab
Eric Gray <ericlamargray@gmail.com>
2020-12-28 12:24:21 -0500

    Added project01

---
+++ project02.js
@@ -0,0 +1,9 @@
+/*
+  In project one you listed the 6 main data types.
+  demonstrate how to use at least 3 JS built in methods
+  on those data types.
+
+  For Example. 
+  let name = "xavier"
+  name.toUpperCase()
+ */
